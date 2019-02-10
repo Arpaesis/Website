4240eab64eb3af34ea3d61efe3e642e6e361e17e
Arpaesis <arpaesis@gmail.com>
2019-02-10 16:47:42 -0500

    Simplified some code.

---
+++ js/navbar.js
@@ -0,0 +1,41 @@
+document.write(
+`
+<div class="fixed-top">
+	  <nav class="navbar">
+		<a href="/index.html" class="logo"><img src="/res/zerra.png" id="logo"></a>
+		<div id="nav">
+			<button class="navbar-toggler zoom" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+			  <span class="navbar-toggler-icon"></span>
+			</button>
+		</div>
+	  </nav>
+  <div class="collapse" id="navbarToggleExternalContent">
+    <div class="p-3">
+	  <div class="row">
+		  <div class="col">
+			<h5>Downloads</h5>
+			<a href="/content/downloads/launcher.html" class="zoom">Launcher</a>
+			<a href="/content/downloads/mods.html" class="zoom">Featured Mods</a>
+			<a href="/content/downloads/worlds.html" class="zoom">Featured Worlds</a>
+		  </div>
+		  <div class="col">
+			<h5>About</h5>
+			<a href="/content/about/zerra.html" class="zoom">About Zerra</a>
+			<a href="/content/about/team.html" class="zoom">Meet the Team</a>
+			<a href="/content/about/eula.html" class="zoom">EULA</a>
+		  </div>
+		  <div class="col">
+			<h5>Support</h5>
+			<a href="/content/support/email.html" class="zoom">Email Us</a>
+			<a href="/content/support/twitter.html" class="zoom">Twitter Support</a>
+			<a href="https://discord.gg/5CUa6qv" class="zoom">Discord Support</a>
+		  </div>
+		  <div class="col">
+			<h5>Account</h5>
+			<a href="/content/account/settings.html" class="zoom">Settings</a>
+		  </div>
+		</div>
+	</div>
+    </div>
+</div>
+`);
